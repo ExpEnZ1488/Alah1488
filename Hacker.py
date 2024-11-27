f3d148ab8cd218bea1c31ce9342469b5d2059065
ExpEnZ1488 <baranovmatvey2281@gmail.com>
2024-11-27 17:00:21 +0200

    Create Hacker.py

---
+++ Hacker.py
@@ -0,0 +1,17 @@
+login = input('Enetr your email')
+password = input('Eneter your password')
+
+right_login = 'musulman52'
+right password = 69
+
+for t in range(10):
+    for  f in  range(10):
+print(pas)
+if pas == right_password:
+   print('Hello musulman52 wanna terror charge today! Pass: ', pas);
+   break
+if pas == right_password:
+  print('Hello musulman52 wanna terror charge today! Pass: ', pas);
+break
+
+
